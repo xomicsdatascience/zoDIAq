cf5598dbda64d857eea1c8529e7ff528047781b4
AlexandreHutton <50920802+AlexandreHutton@users.noreply.github.com>
2022-09-20 11:26:51 -0700

    added versioning tidbit; added missed dependency

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 import setuptools
-
+import os
 
 def readfile(filename):
     with open(filename, 'r+') as f:
         return f.read()
 
 
+def get_version(module_path: str) -> str:
+    """Gets the version from the specified file. Expected to be of format __version__ = 'x.y.z'"""
+    f = open(module_path, 'r')
+    lines = f.read().splitlines()
+    for l in lines:
+        if l.startswith('__version__'):
+            # Found version; parse
+            return l.split('=')[-1].strip().strip("'").strip('"')
+
+pwd = os.path.abspath(os.path.dirname(__file__))
+version = get_version(os.path.join(pwd, 'csodiaq/__init__.py'))
+
 setuptools.setup(
     name="csodiaq",
-    version="0.0.1",
+    version=version,
     author="Meyer Lab",
     author_email="",
     description="CsoDIAq Package",
@@ -21,8 +33,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
     ],
-
-    # packages=['csodiaq'],
     packages=setuptools.find_packages(),
     python_requires='>=3.8',
     license="MIT",
@@ -39,6 +49,7 @@ setuptools.setup(
         'pandas>=1.2.2',
         'Bio>=0.4.1',
         'PyQt5>=5.15.4',
-        'lxml>=4.6.2'
+        'lxml>=4.6.2',
+        'scikit-learn>=1.1.2'
     ],
 )
