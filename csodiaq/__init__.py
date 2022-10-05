f38533dd05da6d1cdc691f09eb6cbd0395e773a1
Lex <50920802+AlexandreHutton@users.noreply.github.com>
2022-10-04 18:05:01 -0700

    added plotting functions for spectra and spectra comparison

--- csodiaq/__init__.py
+++ csodiaq/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.6'
+__version__ = '1.1.7'
 
 from csodiaq import csodiaq_gui, csodiaq, csodiaq_identification_functions, csodiaq_mgf_cleaning_functions, \
     csodiaq_quantification_functions, idpicker, IdentificationSpectraMatcher, spectra_matcher_functions
