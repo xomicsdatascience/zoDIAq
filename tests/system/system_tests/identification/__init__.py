d666aee598abf8d1bbd1e8b2727f1fd1b5418db6
CCranney <11773171+CCranney@users.noreply.github.com>
2023-08-23 10:37:49 -0700

    identification test file saving function made, scoring test files initialized

---
+++ tests/system/system_tests/identification/__init__.py
@@ -0,0 +1,20 @@
+from .testFileContentCreators.libraryCreatingFunctions import (
+    create_template_library_dataframe,
+    make_mgf_library_from_template_library_dataframe,
+    spectrastColumns,
+)
+
+from .testFileContentCreators.BaselineSpectraBreakdown import BaselineSpectraBreakdown
+from .testFileContentCreators.NoMatchSpectraBreakdown import NoMatchSpectraBreakdown
+from .testFileContentCreators.NoCompensationVoltageSpectraBreakdown import (
+    NoCompensationVoltageSpectraBreakdown,
+)
+from .testFileContentCreators.MatchToleranceSpectraBreakdown import (
+    MatchToleranceSpectraBreakdown,
+)
+from .testFileContentCreators.StDevCorrectionSpectraBreakdown import (
+    StDevCorrectionSpectraBreakdown,
+)
+from .testFileContentCreators.CustomCorrectionSpectraBreakdown import (
+    CustomCorrectionSpectraBreakdown,
+)
--- tests/system/system_tests/identification/test_identification.py
+++ tests/system/system_tests/identification/test_identification.py
@@ -7,8 +7,9 @@ import numpy as np
 import pytest
 import re
 from pyteomics import mgf
+import shutil
 
-from . import (
+from tests.system.system_tests.identification import (
     create_template_library_dataframe,
     make_mgf_library_from_template_library_dataframe,
     spectrastColumns,
@@ -38,6 +39,8 @@ def assert_pandas_dataframes_are_equal(expectedDf, df):
             np.testing.assert_array_almost_equal(expectedColumn, column)
         else:
             np.testing.assert_array_equal(expectedColumn, column)
+
+isSavingTestFiles = False
 
 
 @pytest.fixture(scope="module")
@@ -81,9 +84,14 @@ def inputFileDirectory(systemTestFileDirectory):
     os.mkdir(inputDirectory)
     return inputDirectory
 
+@pytest.fixture(scope="module")
+def expectedOutputDirectory(systemTestFileDirectory):
+    expectedOutputDirectory = os.path.join(systemTestFileDirectory.name, "expected_output_files")
+    os.mkdir(expectedOutputDirectory)
+    return expectedOutputDirectory
 
 def test__identification__baseline_run__spectrast_library(
-    libraryTemplateDataFrame, libraryFileDirectory, inputFileDirectory
+    libraryTemplateDataFrame, libraryFileDirectory, inputFileDirectory, expectedOutputDirectory
 ):
     baselineSpectraBreakdown = BaselineSpectraBreakdown(libraryTemplateDataFrame)
     inputFileHeader = "baseline_run_spectrast"
@@ -92,6 +100,7 @@ def test__identification__baseline_run__spectrast_library(
     )
     inputQueryFile = os.path.join(inputFileDirectory, f"{inputFileHeader}.mzXML")
     libraryFile = os.path.join(libraryFileDirectory, "spectrast_test_library.csv")
+    baselineSpectraBreakdown.expectedOutputDf.to_csv(os.path.join(expectedOutputDirectory, f"{inputFileHeader}.csv"), index=False)
     outputDir = TemporaryDirectory(prefix="csodiaq_system_test")
     args = [
         "csodiaq",
@@ -123,7 +132,7 @@ def test__identification__baseline_run__spectrast_library(
 
 
 def test__identification__baseline_run__mgf_library(
-    libraryTemplateDataFrame, libraryFileDirectory, inputFileDirectory
+    libraryTemplateDataFrame, libraryFileDirectory, inputFileDirectory, expectedOutputDirectory
 ):
     baselineSpectraBreakdown = BaselineSpectraBreakdown(libraryTemplateDataFrame)
     inputFileHeader = "baseline_run_mgf"
@@ -132,7 +141,7 @@ def test__identification__baseline_run__mgf_library(
     )
     inputQueryFile = os.path.join(inputFileDirectory, f"{inputFileHeader}.mzXML")
     libraryFile = os.path.join(libraryFileDirectory, "mgf_test_library.mgf")
-
+    baselineSpectraBreakdown.expectedOutputDf.to_csv(os.path.join(expectedOutputDirectory, f"{inputFileHeader}.csv"), index=False)
     outputDir = TemporaryDirectory(prefix="csodiaq_system_test")
     args = [
         "csodiaq",
@@ -224,7 +233,7 @@ def test__identification__baseline__two_inputs_creates_two_outputs_independent_o
 
 
 def test__identification__baseline_no_matches_raises_error(
-    libraryTemplateDataFrame, libraryFileDirectory, inputFileDirectory
+    libraryTemplateDataFrame, libraryFileDirectory, inputFileDirectory, expectedOutputDirectory
 ):
     noMatchSpectraBreakdown = NoMatchSpectraBreakdown(libraryTemplateDataFrame)
     noMatchFileHeader = "no_match_input"
@@ -239,6 +248,8 @@ def test__identification__baseline_no_matches_raises_error(
     inputQueryFile = os.path.join(inputFileDirectory, f"{inputFileHeader}.mzXML")
     noMatchQueryFile = os.path.join(inputFileDirectory, f"{noMatchFileHeader}.mzXML")
     libraryFile = os.path.join(libraryFileDirectory, "spectrast_test_library.csv")
+    baselineSpectraBreakdown.expectedOutputDf.to_csv(os.path.join(expectedOutputDirectory, f"{inputFileHeader}.csv"), index=False)
+    noMatchSpectraBreakdown.expectedOutputDf.to_csv(os.path.join(expectedOutputDirectory, f"{noMatchFileHeader}.csv"), index=False)
     outputDir = TemporaryDirectory(prefix="csodiaq_system_test")
     assert_no_matches_before_correction_raises_warning_and_skips_offending_file_only(
         inputQueryFile,
@@ -307,7 +318,7 @@ def assert_no_matches_after_correction_raises_warning(
 
 
 def test__identification__no_compensation_voltage_succeeds(
-    libraryTemplateDataFrame, libraryFileDirectory, inputFileDirectory
+    libraryTemplateDataFrame, libraryFileDirectory, inputFileDirectory, expectedOutputDirectory
 ):
     noCompensationVoltageSpectraBreakdown = NoCompensationVoltageSpectraBreakdown(
         libraryTemplateDataFrame
@@ -318,6 +329,7 @@ def test__identification__no_compensation_voltage_succeeds(
     )
     inputQueryFile = os.path.join(inputFileDirectory, f"{inputFileHeader}.mzXML")
     libraryFile = os.path.join(libraryFileDirectory, "spectrast_test_library.csv")
+    noCompensationVoltageSpectraBreakdown.expectedOutputDf.to_csv(os.path.join(expectedOutputDirectory, f"{inputFileHeader}.csv"), index=False)
     outputDir = TemporaryDirectory(prefix="csodiaq_system_test")
     args = [
         "csodiaq",
@@ -583,3 +595,12 @@ def test__identification__stress_test_memory():
         outputDir.name,
     ]
     subprocess.run(args)
+
+@pytest.mark.skip(
+    "Test files are temporarily made on-the-fly, but there are instances where one would want to investigate specific file outputs. This test function saves the test files to a permanent directory. Remove the skip tag if you want to save the files for in-depth data interrogation."
+)
+def test__identification__make_permanent_copy_of_test_files(systemTestFileDirectory):
+    parentDir =os.path.dirname(systemTestFileDirectory.name)
+    permanentTestDirectory = os.path.join(parentDir, 'csodiaq_system_test_files')
+    shutil.copytree(systemTestFileDirectory.name, permanentTestDirectory)
+    print(f'\nsaved test files to {permanentTestDirectory}')
