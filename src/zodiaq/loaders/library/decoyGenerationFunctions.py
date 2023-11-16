b3d8d3c828c75c49093cee8d3185f18d03aacdea
CCranney <11773171+CCranney@users.noreply.github.com>
2023-11-15 19:31:14 -0800

    refactor decoy generation functions

--- src/zodiaq/loaders/library/decoyGenerationFunctions.py
+++ src/zodiaq/loaders/library/decoyGenerationFunctions.py
@@ -1,30 +1,39 @@
 import random
-nonKRAminoAcids = [ "A", "N", "D", "C", "E", "Q", "G", "H", "I", "L", "M", "F", "S", "T", "W", "Y", "V"]
+nonCleavageAminoAcids = [ "A", "N", "D", "C", "E", "Q", "G", "H", "I", "L", "M", "F", "S", "T", "W", "Y", "V"]
+cleavageAminoAcids = set(['K', 'P', 'R'])
 
 def shuffle_peptide_sequence_with_preserved_cleavage_points(peptide):
-    cleavageAAs = set(['K','P','R'])
-    cleavageAALocations = [(i-len(peptide)+1,char) for (i, char) in enumerate(peptide) if char in cleavageAAs]
-    otherAAs = [char for char in peptide if char not in cleavageAAs]
-    shuffledPeptide = ''
+    cleavageAALocations = [(i-len(peptide)+1,char) for (i, char) in enumerate(peptide) if char in cleavageAminoAcids]
+    otherAAs = [char for char in peptide if char not in cleavageAminoAcids]
     for i in range(100):
-        shuffledPeptide = otherAAs[:]
-        random.shuffle(shuffledPeptide)
-        if i % 10 == 0:
-            randomIdx = random.randint(0,len(shuffledPeptide)-1)
-            shuffledPeptide[randomIdx] = random.choice(nonKRAminoAcids)
-        shuffledPeptide[0]
-        for j in range(len(cleavageAALocations)-1, -1, -1):
-            cleavageAAIndex = cleavageAALocations[j][0]
-            if not cleavageAAIndex:
-                cleavageAAIndex = len(peptide)
-            cleavageAA = cleavageAALocations[j][1]
-            shuffledPeptide.insert(cleavageAAIndex, cleavageAA)
-        shuffledPeptideString = ''.join(shuffledPeptide)
+        shuffledPeptide = shuffle_non_cleavage_amino_acids(otherAAs, i)
+        shuffledPeptideString = insert_cleavage_amino_acids_into_shuffled_peptide(shuffledPeptide, cleavageAALocations, peptide)
         if calculate_similarities_between_strings(peptide, shuffledPeptideString) < 0.7:
             return shuffledPeptideString
     if calculate_similarities_between_strings(peptide, shuffledPeptideString) >= 0.7:
         return peptide
     return shuffledPeptideString
+
+def shuffle_non_cleavage_amino_acids(otherAAs, i):
+    shuffledPeptide = otherAAs[:]
+    random.shuffle(shuffledPeptide)
+    if i % 10 == 0:
+        insert_random_amino_acid_to_shuffled_peptide_to_increase_variability(shuffledPeptide)
+    return shuffledPeptide
+
+def insert_random_amino_acid_to_shuffled_peptide_to_increase_variability(shuffledPeptide):
+    randomIdx = random.randint(0, len(shuffledPeptide) - 1)
+    shuffledPeptide[randomIdx] = random.choice(nonCleavageAminoAcids)
+    return shuffledPeptide
+
+def insert_cleavage_amino_acids_into_shuffled_peptide(shuffledPeptide, cleavageAALocations, peptide):
+    for j in range(len(cleavageAALocations) - 1, -1, -1):
+        cleavageAAIndex = cleavageAALocations[j][0]
+        if not cleavageAAIndex:
+            cleavageAAIndex = len(peptide)
+        cleavageAA = cleavageAALocations[j][1]
+        shuffledPeptide.insert(cleavageAAIndex, cleavageAA)
+    return ''.join(shuffledPeptide)
 
 def insert_value(string, index, char):
     return string[:index] + [char] + string[index+1:]
