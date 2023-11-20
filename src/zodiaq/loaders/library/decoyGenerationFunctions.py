6f87f635814a28530578e2e2faafe314c2b95c8c
CCranney <11773171+CCranney@users.noreply.github.com>
2023-11-20 15:51:14 -0800

    decoy generation integrated with zodiaq library dictionary; shuffling/ion mz calculations includes unimod modifications

--- src/zodiaq/loaders/library/decoyGenerationFunctions.py
+++ src/zodiaq/loaders/library/decoyGenerationFunctions.py
@@ -1,30 +1,34 @@
+import re
 import random
 from Bio.Seq import Seq
 from Bio.SeqUtils import molecular_weight
-
+from zodiaq.utils import format_protein_string_to_list, format_protein_list_to_string
+from zodiaq.loaders.library.modificationMassDict import modificationMassDict
 nonCleavageAminoAcids = [ "A", "N", "D", "C", "E", "Q", "G", "H", "I", "L", "M", "F", "S", "T", "W", "Y", "V"]
 cleavageAminoAcids = set(['K', 'P', 'R'])
+modificationRegexStr = r'(.(?:[\[\(][^\)\]]+[\]\)])?)'
 
 def shuffle_peptide_sequence_with_preserved_cleavage_points(peptide):
-    cleavageAALocations = [(i-len(peptide)+1,char) for (i, char) in enumerate(peptide) if char in cleavageAminoAcids]
-    otherAAs = [char for char in peptide if char not in cleavageAminoAcids]
+    originalPeptide = re.findall(modificationRegexStr, peptide)
+    cleavageAALocations = [(i-len(originalPeptide)+1,char) for (i, char) in enumerate(originalPeptide) if char in cleavageAminoAcids]
+    otherAAs = [char for char in originalPeptide if char not in cleavageAminoAcids]
     for i in range(100):
         shuffledPeptide = shuffle_non_cleavage_amino_acids(otherAAs, i)
-        shuffledPeptideString = insert_cleavage_amino_acids_into_shuffled_peptide(shuffledPeptide, cleavageAALocations, peptide)
-        if calculate_similarities_between_strings(peptide, shuffledPeptideString) < 0.7:
-            return shuffledPeptideString
-    if calculate_similarities_between_strings(peptide, shuffledPeptideString) >= 0.7:
+        shuffledPeptide = insert_cleavage_amino_acids_into_shuffled_peptide(shuffledPeptide, cleavageAALocations, originalPeptide)
+        if calculate_similarities_between_strings(originalPeptide, shuffledPeptide) < 0.7:
+            return ''.join(shuffledPeptide)
+    if calculate_similarities_between_strings(originalPeptide, shuffledPeptide) >= 0.7:
         return peptide
-    return shuffledPeptideString
+    return ''.join(shuffledPeptide)
 
 def shuffle_non_cleavage_amino_acids(otherAAs, i):
     shuffledPeptide = otherAAs[:]
     random.shuffle(shuffledPeptide)
     if i % 10 == 0:
-        insert_random_amino_acid_to_shuffled_peptide_to_increase_variability(shuffledPeptide)
+        randomly_swap_single_amino_acid_in_shuffled_peptide_to_increase_variability(shuffledPeptide)
     return shuffledPeptide
 
-def insert_random_amino_acid_to_shuffled_peptide_to_increase_variability(shuffledPeptide):
+def randomly_swap_single_amino_acid_in_shuffled_peptide_to_increase_variability(shuffledPeptide):
     randomIdx = random.randint(0, len(shuffledPeptide) - 1)
     shuffledPeptide[randomIdx] = random.choice(nonCleavageAminoAcids)
     return shuffledPeptide
@@ -36,7 +40,7 @@ def insert_cleavage_amino_acids_into_shuffled_peptide(shuffledPeptide, cleavageA
             cleavageAAIndex = len(peptide)
         cleavageAA = cleavageAALocations[j][1]
         shuffledPeptide.insert(cleavageAAIndex, cleavageAA)
-    return ''.join(shuffledPeptide)
+    return shuffledPeptide
 
 def insert_value(string, index, char):
     return string[:index] + [char] + string[index+1:]
@@ -56,15 +60,59 @@ def calculate_ion_mz(sequence, type, position, charge):
 
 
 def determine_terminal_end_of_sequence(sequence, type, position):
+    modifiedSequence = re.findall(modificationRegexStr, sequence)
     if type == 'y':
-        return Seq(sequence[-position:])
+        return ''.join(modifiedSequence[-position:])
     else:
-        return Seq(sequence[:position])
+        return ''.join(modifiedSequence[:position])
 
 
 def calculate_molecular_mass_of_sequence(sequence, type, charge):
     H2O = 18.01468
-    mass = molecular_weight(sequence, seq_type='protein')
+    modifications = re.findall(r'([\[\(][^\)\]]+[\]\)])', sequence)
+    strippedSequence = re.sub(r'([\[\(][^\)\]]+[\]\)])', '', sequence)
+    mass = molecular_weight(Seq(strippedSequence), seq_type='protein')
+    modificationMass = sum([modificationMassDict[m] for m in modifications])
     if type == 'b':
         mass -= H2O
-    return mass + charge
+    return mass + charge + modificationMass
+
+def add_decoys_to_zodiaq_library(zodiaqLibraryDict):
+    decoyDict = {}
+    for key, value in zodiaqLibraryDict.items():
+        decoyKey, decoyValue = create_decoy_from_target_in_zodiaq_library(key, value)
+        if decoyKey:
+            decoyDict[decoyKey] = decoyValue
+    outputDict = recalculate_zodiaq_library_key_idx_to_account_for_inserted_decoys(decoyDict | zodiaqLibraryDict)
+    return outputDict
+
+def create_decoy_from_target_in_zodiaq_library(targetKey, targetValue):
+    targetPeptide = targetKey[1]
+    targetPeaks = targetValue['peaks']
+    fragmentTypes = targetValue['fragmentTypes']
+    decoyPeptide = shuffle_peptide_sequence_with_preserved_cleavage_points(targetPeptide)
+    if decoyPeptide == targetPeptide:
+        return None, None
+    decoyMzs = [calculate_ion_mz(decoyPeptide, *fragmentType) for fragmentType in fragmentTypes]
+    decoyPeaks = [(decoyMzs[i], targetPeaks[i][1], -1) for i in range(len(targetPeaks))]
+    targetProteinString = targetValue['proteinName']
+    decoyProteins = format_protein_string_to_list(targetProteinString)
+    decoyProteins = [f'DECOY_{protein}' for protein in decoyProteins]
+    decoyProteinString = format_protein_list_to_string(decoyProteins)
+    decoyValue = targetValue
+    decoyValue['identification'] = f'{decoyPeptide}_{targetKey[0]}_DECOY'
+    decoyValue['proteinName'] = decoyProteinString
+    decoyValue['peaks'] = decoyPeaks
+    decoyValue['isDecoy'] = 1
+    decoyValue['zodiaqKeyIdx'] = -1
+    return (targetKey[0], decoyPeptide), decoyValue
+
+def recalculate_zodiaq_library_key_idx_to_account_for_inserted_decoys(outputDict):
+    sortedKeys = sorted(outputDict)
+    for i in range(len(sortedKeys)):
+        key = sortedKeys[i]
+        value = outputDict[key]
+        value['peaks'] = [(x[0],x[1],i) for x in value['peaks']]
+        value['zodiaqKeyIdx'] = i
+        outputDict[key] = value
+    return outputDict
